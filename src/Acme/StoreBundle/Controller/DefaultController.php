0328238b6c170a46f3f27d0c9f3029a4c6a17b1d
Geslain <geslain.dahan@gmail.com>
2016-04-01 12:43:39 +0200

    templates

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -20,4 +20,48 @@ class DefaultController extends Controller
 
         return $this->render('AcmeStoreBundle:default:index.html.twig',array("user"=>$user));
     }
+
+    /**
+     * @Route("/players")
+     */
+    public function viewPlayersAction()
+    {
+        $userRepository = $this->get('doctrine_mongodb')->getRepository("AcmeStoreBundle:User");
+        $users = $userRepository->findAll();
+
+        return $this->render('AcmeStoreBundle:default:players.html.twig',array("users"=>$users));
+    }
+
+    /**
+     * @Route("/player/{name}")
+     */
+    public function viewPlayerAction($name)
+    {
+        $userRepository = $this->get('doctrine_mongodb')->getRepository("AcmeStoreBundle:User");
+        $users = $userRepository->findOneByUsername($name);
+
+        return $this->render('AcmeStoreBundle:default:player.html.twig',array("user"=>$users));
+    }
+
+    /**
+     * @Route("/games")
+     */
+    public function viewGames()
+    {
+        $gameRepository = $this->get('doctrine_mongodb')->getRepository("AcmeStoreBundle:Game");
+        $games = $gameRepository->findAll();
+
+        return $this->render('AcmeStoreBundle:default:games.html.twig',array("games"=>$games));
+    }
+
+    /**
+     * @Route("/game/{name}")
+     */
+    public function viewGame($name)
+    {
+        $gameRepository = $this->get('doctrine_mongodb')->getRepository("AcmeStoreBundle:Game");
+        $game = $gameRepository->findOneByName($name);
+
+        return $this->render('AcmeStoreBundle:default:game.html.twig',array("game"=>$game));
+    }
 }
